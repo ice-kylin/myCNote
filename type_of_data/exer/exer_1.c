78cdead8412a385d2c81dad85b6645654caa4471
ice-kylin <wminid@yeah.net>
2022-04-24 10:34:13 +0100

    update

---
+++ type_of_data/exer/exer_1.c
@@ -0,0 +1,18 @@
+//
+// Created by icekylin on 4/23/22.
+//
+
+//#include <stdio.h>
+
+//int main() {
+//    char name[] = "icekylin";
+//    int age = 19;
+//    double grade = 95.5;
+//    char gender[] = "女";
+//    char hobby[] = "play";
+//
+//    printf("姓名\t\t年龄\t成绩\t性别\t爱好\n");
+//    printf("%s\t%d\t%.1f\t%s\t%s\n", name, age, grade, gender, hobby);
+//
+//    return 0;
+//}
--- type_of_data/exer/pointer.c
+++ type_of_data/exer/pointer.c
@@ -2,22 +2,22 @@
 // Created by icekylin on 2022-04-23.
 //
 
-#include <stdio.h>
+//#include <stdio.h>
 
-int main() {
-    // 获取一个 double 变量 num 的地址，并显示到终端
-    double num = 666.666;
-
-    printf("num = %f\n", num);
-    printf("&num = %p\n\n", &num);
-
-    // 将 num 的地址赋给指针 ptr，并通过 ptr 去修改 num 的值
-    double *ptr = &num;
-    *ptr = 233.333;
-
-    printf("ptr = %p\n", ptr);
-    printf("num = %f\n", num);
-    printf("*ptr = %f\n", *ptr);
-
-    return 0;
-}
+//int main() {
+//    // 获取一个 double 变量 num 的地址，并显示到终端
+//    double num = 666.666;
+//
+//    printf("num = %f\n", num);
+//    printf("&num = %p\n\n", &num);
+//
+//    // 将 num 的地址赋给指针 ptr，并通过 ptr 去修改 num 的值
+//    double *ptr = &num;
+//    *ptr = 233.333;
+//
+//    printf("ptr = %p\n", ptr);
+//    printf("num = %f\n", num);
+//    printf("*ptr = %f\n", *ptr);
+//
+//    return 0;
+//}
